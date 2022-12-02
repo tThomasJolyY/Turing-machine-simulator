3f8f54285903ae5d662fe932f66fedacf6e98929
uvsq21916099 <thomas.joly@ens.uvsq.fr>
2022-12-02 15:58:57 -0500

    fonctionel avec 1 ruban pas par pas

--- mt.py
+++ mt.py
@@ -1,15 +1,62 @@
-print("La machine de fou")
-print("test")
+from recup_mt import create_dic
 
 class Machine:
-    def __init__(self) -> None:
+    def __init__(self,fichier) -> None:
+        self.dic_etatstrans = create_dic(fichier)
+        self.nb_rubans = 0
+        self.etat_ruban = ""
+        self.etat_courant = "I"
+        self.position = 0
+        self.nb_pas = 0
+        self.mot = ""
+
+    def calc_nb_rubans(self):
+        #for elt in self.dic_etatstrans.items():
+         #   nb_rubans = len(elt[0]) - 1
         pass
 
-    def etat_courant():
+    def get_etat_courant(self):
+        return self.etat
+
+    def get_etat_ruban(self):
+        return self.etat_ruban
+
+    def position_lecture(self):
+        return self.position
+
+    def get_transitions(self):
         pass
 
-    def etat_bande():
-        pass
+    def get_dic(self):
+        return self.dic_etatstrans
 
-    def position_lecture():
-        pass
+    def entrée(self,mot):
+        self.mot = list(mot)
+        self.etat_ruban = self.mot[0]
+
+    def __str__(self):
+        l = "".join(str([self.mot,self.etat_ruban,self.position,self.etat_courant,self.nb_pas]))
+        return str(l)
+
+    def pas(self):
+        if self.mot == "":
+            print("Veuillez spécifier une entrée")
+        for elt in self.dic_etatstrans.items():
+            etat = elt[0]
+            transi = elt[1]
+            if etat[0] == self.etat_courant:
+                if self.etat_ruban == etat[1]:
+                    self.etat_courant = transi[0]
+                    self.mot[self.position] = transi[1][0]
+                    if transi[2][0] == ">":
+                        self.position += 1
+                    if transi[2][0] == "<":
+                        self.position -= 1
+                    self.etat_ruban = self.mot[self.position]
+                    self.nb_pas += 1
+            
+
+mt_test = Machine("mt.txt")
+mt_test.entrée("000")
+mt_test.pas()
+print(mt_test)
---
+++ recup_mt.py
@@ -0,0 +1,56 @@
+MOVES = [">","<","-"]
+
+def create_dic(path):
+    with open(path) as f:
+        l = f.readlines()
+        f.close()
+
+    etats_transitions = {}
+
+    liste_etats = []
+    liste_transitions = []
+
+    lecture_etat = True
+    lecture_transition = False
+
+    for ligne in l:
+        if len(ligne) > 1 and lecture_etat:
+            etat = []
+            car = []
+            i = 0
+            while ligne[i] != ",":
+                etat.append(ligne[i])
+                i+=1
+            i+=1
+            while i < len(ligne)-1:
+                if ligne[i] != ",":
+                    car.append(ligne[i])
+                i+=1
+            lecture_etat = False
+            lecture_transition = True
+            liste_etats.append(["".join(etat),"".join(car)])
+        elif len(ligne) > 1 and lecture_transition:
+            etatnv = []
+            carnv = []
+            mouv = []
+            i = 0
+            while ligne[i] != ",":
+                etatnv.append(ligne[i])
+                i+=1
+            i+=1
+            while ligne[i+1] not in MOVES:
+                if ligne[i] != ",":
+                    carnv.append(ligne[i])
+                i+=1
+            i+=1
+            while i < len(ligne)-1 :
+                mouv.append(ligne[i])
+                i+=1
+            lecture_etat = True
+            lecture_transition = False
+            liste_transitions.append(["".join(etatnv),carnv,mouv])
+
+    for k in range(len(liste_etats)):
+        etats_transitions["".join(liste_etats[k])] = liste_transitions[k]
+    return etats_transitions
+
