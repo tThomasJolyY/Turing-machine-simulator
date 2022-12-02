f5363c8bc979579f60ccc5896f19aef8f0d35f42
Yanis Kbayli <kbyanis26@gmail.com>
2022-12-02 12:07:40 +0100

    aa

--- mt.py
+++ mt.py
@@ -1,2 +1,15 @@
 print("La machine de fou")
 print("test")
+
+class Machine:
+    def __init__(self) -> None:
+        pass
+
+    def etat_courant():
+        pass
+
+    def etat_bande():
+        pass
+
+    def position_lecture():
+        pass
