9a1513a18c54de4862d53c40e543d09a0e7aeea0
uvsq21916099 <thomas.joly@ens.uvsq.fr>
2022-12-03 03:04:03 +0100

    Correction de bugs et ajout de lancer_machine

--- mt.py
+++ mt.py
@@ -10,6 +10,14 @@ class Machine:
         self.nb_pas = 0
         self.rubans = []
 
+    def reset(self,mot):
+        self.etat_courant = "I"
+        self.etats_rubans = []
+        self.position = []
+        self.nb_pas = 0
+        self.rubans = []
+        self.entrée(mot)        
+
     def calc_nb_rubans(self):
         for elt in self.dic_etatstrans.items():
             for car in range(len(elt[0])):
@@ -34,9 +42,12 @@ class Machine:
 
     def entrée(self,mot):
         self.rubans.append(list(mot))
-        self.rubans.append(["_" for _ in range(self.nb_rubans-1)])
-        self.etats_rubans= ["_" for _ in range(self.nb_rubans)]
-        self.etats_rubans[0] = self.rubans[0][0]
+        if self.nb_rubans > 1:
+            self.rubans.append(["_" for _ in range(self.nb_rubans-1)])
+            self.etats_rubans= ["_" for _ in range(self.nb_rubans)]
+            self.etats_rubans[0] = self.rubans[0][0]
+        else:
+            self.etats_rubans.append(self.rubans[0][0])
         self.position = [0 for _ in range(self.nb_rubans)]
 
     def __str__(self):
@@ -53,16 +64,15 @@ class Machine:
             if etat_courant_comp == self.etat_courant:
                 etats_comp = "".join([etat[car+1:] for car in range(len(etat)) if etat[car] == ","])
                 if "".join(self.etats_rubans) == etats_comp:
-                    self.etat_courant = transi[0] #ca c carré
+                    self.etat_courant = transi[0]
                     for ruban_i in range(self.nb_rubans):
                         self.rubans[ruban_i][self.position[ruban_i]] = transi[1][ruban_i] 
-                        print(self.rubans[ruban_i])
-                    for transition in range(self.nb_rubans):    
+                    for transition in range(self.nb_rubans): 
                         if transi[2][transition] == ">":
                             self.position[transition] += 1
                             if len(self.rubans[transition]) <= self.position[transition]:
                                 self.rubans[transition].append("_")
-                        if transi[2][transition] == "<": #marche pas wlh
+                        if transi[2][transition] == "<": 
                             self.position[transition] -= 1
                             if self.position[transition] < 0:
                                 old_rub = [elt for elt in self.rubans[transition]]
@@ -74,14 +84,23 @@ class Machine:
                     for etat_rub in range(self.nb_rubans):
                         self.etats_rubans[etat_rub] = self.rubans[etat_rub][self.position[etat_rub]]
                     self.nb_pas += 1
-                print(mt_test)
-                return True
+                #print(mt_test)
+                    return True
         return False
-            
+        
+    def lancer_machine(self):
+        while self.etat_courant != "F":
+            res = self.pas()
+            if not res:
+                print("Le mot que vous avez entré n'est pas reconnu par la machine de turing")
+                return False
+        print("Voici l'état de la machine dans son état finale")
+        print(self)
+        return True
 
 mt_test = Machine("mt.txt")
-mt_test.entrée("000")
+mt_test.entrée("100")
+mt_test.lancer_machine()
+mt_test.reset("001")
+mt_test.pas()
 print(mt_test)
-mt_test.pas()
-mt_test.pas()
-mt_test.pas()
